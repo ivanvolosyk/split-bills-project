b8f229d267e884e2c709c3bd8e1620eda2ddc6dd
Ivan Volosyk <volosyk.vanya@gmail.com>
2025-03-25 00:19:25 +0200

    Final project

--- src/App.js
+++ src/App.js
@@ -51,6 +51,18 @@ export default function App() {
     );
     setShowAddFriend(false);
   }
+
+  function handlerSplitBill(value) {
+    setFriends((friends) =>
+      friends.map((friend) =>
+        friend.id === selectedFriend.id
+          ? { ...friend, balance: friend.balance + value }
+          : friend
+      )
+    );
+
+    setSelectedFriend(null);
+  }
   return (
     <div className="app">
       <div className="sidebar">
@@ -66,7 +78,12 @@ export default function App() {
           {showAddFriend ? "Close" : "Add friend"}
         </Button>
       </div>
-      {selectedFriend && <FormSplitBill selectedFriend={selectedFriend} />}
+      {selectedFriend && (
+        <FormSplitBill
+          selectedFriend={selectedFriend}
+          onSplitBill={handlerSplitBill}
+        />
+      )}
     </div>
   );
 }
@@ -152,14 +169,20 @@ function FormAddFriend({ onAddFriend }) {
   );
 }
 
-function FormSplitBill({ selectedFriend }) {
+function FormSplitBill({ selectedFriend, onSplitBill }) {
   const [bill, setBill] = useState("");
   const [paidByUser, setPaidByUser] = useState("");
   const paidByFriend = bill ? bill - paidByUser : "";
   const [whoIsPaying, setWhoIsPaying] = useState("user");
 
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!bill || !paidByUser) return;
+    onSplitBill(whoIsPaying === "user" ? paidByFriend : -paidByUser);
+  }
   return (
-    <form className="form-split-bill">
+    <form className="form-split-bill" onSubmit={handleSubmit}>
       <h2>{`Split a bill with ${selectedFriend.name}`} </h2>
 
       <label>💰 Bill value</label>
@@ -185,7 +208,7 @@ function FormSplitBill({ selectedFriend }) {
 
       <label>🤑 Who is paying the bill</label>
       <select
-        whoIsPaying={bill}
+        value={whoIsPaying}
         onChange={(e) => setWhoIsPaying(e.target.value)}
       >
         <option value="user">You</option>
